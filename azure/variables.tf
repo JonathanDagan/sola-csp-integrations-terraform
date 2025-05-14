e0e5c96e5fdb5be4708e3015ad0000f31492d5b6
JonathanDagan <55870920+JonathanDagan@users.noreply.github.com>
2025-05-14 14:42:40 +0300

    switch to custom role

--- azure/variables.tf
+++ azure/variables.tf
@@ -8,3 +8,15 @@ variable "app_name" {
   default     = "sola-ro-integration"
   description = "Application subscription name"
 }
+
+variable "azure_wait_timer" {
+  type        = string
+  description = "(Optional) Wait timer for Azure dataplane propagation - Default: 360"
+  default     = "360s"
+}
+
+variable "custom_role_name" {
+  type        = string
+  default     = "Sola Custom Role"
+  description = "The name of the Azure custom role to create. Defaults to Sola Custom Role."
+}
